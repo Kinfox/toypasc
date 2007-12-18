e602659da02f60b7f9740fcf9e10ac4dafcd5e21
felipebart <felipebart@cbd757d2-8441-0410-81db-47b75ec2d254>
2007-12-18 14:28:58 +0000

    Tabela de simbolos... Construção das funções.

--- symbol_table.c
+++ symbol_table.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include "symbol_table.h"
 
+
 Symbol *
 symbol_insert(Symbol * table, char const * name, int type)
 {
@@ -10,9 +11,11 @@ symbol_insert(Symbol * table, char const * name, int type)
 
     symbol = (Symbol *) malloc (sizeof(Symbol));
     symbol->next = table;
+    symbol->name =  (char *) malloc (sizeof(char)*strlen(name));
     strcpy (symbol->name, name);
     symbol->type = type;
     return symbol;
+    
 }
 
 Symbol *
@@ -20,12 +23,21 @@ symbol_lookup(Symbol * table, char const * name)
 {
     Symbol *temp;
     temp = table;
-
-    while(strcmp (temp->name, name) && temp != NULL){
-        temp = temp->next;
+    
+    while(temp != NULL){
+    
+        if(strcmp (temp->name, name)){
+    
+            return temp;   
+    
+        }
+    
+        temp = temp->next;  
+    
     }
-
+    
     return temp;
+
 }
 
 void
@@ -46,14 +58,26 @@ symbol_table_destroy(Symbol *table)
 }
 
 void
+symbol_print(Symbol *symbol)
+{
+    if (symbol == NULL) {
+        printf("NULL\n");
+        return;
+    }
+    
+    printf("Symbol: %x\n", symbol);
+    printf("%s\n", symbol->name);
+    printf("%d\n", symbol->type);
+}
+
+void
 symbol_table_dump(Symbol *table)
 {
     Symbol *temp;
     temp = table;
 
-    while(temp != NULL){
-        printf("%s\n", temp->name);
-        printf("%d\n", temp->type);
+    while(temp != NULL) {
+        symbol_print(temp);
         temp = temp->next;
     }
 

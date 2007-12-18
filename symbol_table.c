5647c6e1cd1a2115060af7a73907623e14593331
felipebart <felipebart@cbd757d2-8441-0410-81db-47b75ec2d254>
2007-12-18 03:30:49 +0000

    Implementando a symbol_table

--- symbol_table.c
+++ symbol_table.c
@@ -1,28 +1,45 @@
+#include <string.h>
 #include "symbol_table.h"
 
-Symbol *
-symbol_table_new(void)
-{
-}
-
 void
-symbol_table_destroy(void)
+symbol_table_destroy(Symbol * table)
 {
+    Symbol * temp;
+
+    while(table->next != NULL){
+    
+        table
+    
+    }
+
 }
 
 char *
 symbol_table_dump(void)
 {
+
+    
+
 }
 
 Symbol *
-symbol_insert(char const * name, int type)
+symbol_insert(Symbol * table, char const * name, int type)
 {
+
+    Symbol * symbol = (Symbol *) malloc (sizeof(Symbol));
+    symbol->next = table;
+    strcpy (symbol->name,name);
+    symbol->type = type;
+    return symbol;
+
 }
 
 Symbol *
 symbol_lookup(char const * name)
 {
+
+        
+    
 }
 
 int
